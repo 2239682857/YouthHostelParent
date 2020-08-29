14013eec692572e29f758f4440b429a334b5bff1
zou <2239682857@qq.com>
2020-08-29 19:07:29 +0800

    添加Eureka、manage、web模块

---
+++ hostel_manage/src/main/java/com/example/manage/ManageApplication.java
@@ -0,0 +1,18 @@
+package com.example.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 流星紫蝶雨
+ * @version 1.0
+ * @date 2020/8/29 11:42 上午
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ManageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManageApplication.class, args);
+    }
+}
--- hostel_user/src/main/java/com/example/user/UserApplication.java
+++ hostel_user/src/main/java/com/example/user/UserApplication.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
 import util.JwtUtil;
 
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan("com.example.user.mapper")
 public class UserApplication {
 
